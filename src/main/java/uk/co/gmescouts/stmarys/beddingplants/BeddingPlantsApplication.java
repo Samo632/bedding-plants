5ff4a4c764ede3d602976fe2886e781e2e27676f
Samo <cs_sampson@hotmail.com>
2018-04-21 17:54:44 +0100

    Initial commit

---
+++ src/main/java/uk/co/gmescouts/stmarys/beddingplants/BeddingPlantsApplication.java
@@ -0,0 +1,12 @@
+package uk.co.gmescouts.stmarys.beddingplants;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeddingPlantsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeddingPlantsApplication.class, args);
+	}
+}
